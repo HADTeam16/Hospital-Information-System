b72c77c6d78a7fc39dbe01faebe0c577909b5357
rishabh0014 <rishabhteli14@gmail.com>
2024-01-16 23:01:54 +0530

    init

---
+++ Hospital-Information-System/src/test/java/org/example/hospitalinformationsystem/HospitalInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hospitalinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
