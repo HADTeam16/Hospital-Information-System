3eb0ceb762354fa898bacc705861c4e6242cbef4
rishabh0014 <rishabhteli14@gmail.com>
2024-01-16 23:20:49 +0530

    init

---
+++ src/main/java/org/net/hospital_info_sys/HospitalInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.net.hospital_info_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalInformationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/net/hospital_info_sys/HospitalInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.net.hospital_info_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
