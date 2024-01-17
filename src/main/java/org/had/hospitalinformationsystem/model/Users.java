f1ec1caa5ddab46d7c4ad2e358a6060badacfe9d
rishabh0014 <rishabhteli14@gmail.com>
2024-01-17 15:26:29 +0530

    firebase connection

--- src/main/java/org/had/hospitalinformationsystem/model/Users.java
+++ src/main/java/org/had/hospitalinformationsystem/model/Users.java
@@ -1,26 +1,20 @@
 package org.had.hospitalinformationsystem.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Getter
 @Setter
+@Getter
 public class Users {
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+
     private String id;
     private String firstName;
     private String lastName;
-    @Id
     private String userName;
     private String password;
     private Integer age;
     private String role;
     private String contact;
-
+    private String userid;
 }
