9ac7e546b304616ddabff783d90785c5ac009bec
shivamjaiswal424 <shivam424.sj@gmail.com>
2024-01-17 11:02:41 +0530

    added dependencies

---
+++ src/main/java/org/had/hospitalinformationsystem/model/Users.java
@@ -0,0 +1,26 @@
+package org.had.hospitalinformationsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Users {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private String id;
+    private String firstName;
+    private String lastName;
+    @Id
+    private String userName;
+    private String password;
+    private Integer age;
+    private String role;
+    private String contact;
+
+}
