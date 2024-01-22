0aab1f33f3e663b9cd41bf7ce2c3a35fd09a15a2
rishabh0014 <rishabhteli14@gmail.com>
2024-01-22 23:27:30 +0530

    sahi wala

--- src/main/java/org/had/hospitalinformationsystem/config/AppConfig.java
+++ src/main/java/org/had/hospitalinformationsystem/config/AppConfig.java
@@ -1,32 +1,37 @@
 package org.had.hospitalinformationsystem.config;
 
-//import org.apache.tomcat.util.http.parser.Authorization;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//        http.sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(Authorize ->Authorize.requestMatchers("/api/**")
-//                        .authenticated()
-//                        .anyRequest()
-//                        .permitAll())
-//                .addFilterBefore(new jwtValidator(), BasicAuthenticationFilter.class)
-//                .csrf(csrf->csrf.disable())
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize ->Authorize.requestMatchers("/api/")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll())
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf->csrf.disable())
 //                .cors(cors->cors.configurationSource(corsConfigurationSource()));
-//
-//        return http.build();
-//    }
+        ;
 
+        return http.build();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
