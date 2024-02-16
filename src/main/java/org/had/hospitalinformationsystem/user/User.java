1764ba4993a13c16e6916506c334c60852c49f5e
Abhinav Dhoka <abhinavdhoka@gmail.com>
2024-02-16 15:39:27 +0530

    cors added

--- src/main/java/org/had/hospitalinformationsystem/user/User.java
+++ src/main/java/org/had/hospitalinformationsystem/user/User.java
@@ -2,12 +2,14 @@ package org.had.hospitalinformationsystem.user;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Table(name="users")
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -38,6 +40,21 @@ public class User {
     private String emergencyContactNumber;
 
     private String role;
+
+    public User(String userName, String password, String firstName, String middleName, String lastName, Integer age,
+            String gender, String dateOfBirth, String contact, String email, String role) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.contact = contact;
+        this.email = email;
+        this.role = role;
+    }
 
 
 }
