6d94e857266d2b57f71a30ee21f970e37ec076f4
Abhinav Dhoka <abhinavdhoka@gmail.com>
2024-04-09 15:00:05 +0530

    nurse head changes

--- src/main/java/org/had/hospitalinformationsystem/nurse/NurseController.java
+++ src/main/java/org/had/hospitalinformationsystem/nurse/NurseController.java
@@ -18,7 +18,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -41,58 +43,57 @@ public class NurseController {
     @Autowired
     NeedWardService needWardService;
 
-    //this api will help you find patients in needWard table
+    // this api will help you find patients in needWard table
     @GetMapping("patients/who/needs/ward")
-    public ResponseEntity<List<NeedWard>>  patientsNeedWard(@RequestHeader("Authorization") String jwt){
-        String role= JwtProvider.getRoleFromJwtToken(jwt);
+    public ResponseEntity<List<NeedWard>> patientsNeedWard(@RequestHeader("Authorization") String jwt) {
+        String role = JwtProvider.getRoleFromJwtToken(jwt);
 
-        String userName=JwtProvider.getUserNameFromJwtToken(jwt);
-        User user=userRepository.findByUserName(userName);
-        Optional<Nurse> nurse=nurseRepository.findById(user.getId());
-        if(!role.equals("nurse")){
+        String userName = JwtProvider.getUserNameFromJwtToken(jwt);
+        User user = userRepository.findByUserName(userName);
+        Optional<Nurse> nurse = nurseRepository.findById(user.getId());
+        if (!role.equals("nurse")) {
             return ResponseEntity.badRequest().body(null);
 
         }
-        if(nurse.get().isHeadNurse()){
-            List<NeedWard> needWards=needWardRepository.returnNeedWards();
-            List<Patient> patients=nurseService.getPatientsFromNeedWard(needWards);
+        if (nurse.get().isHeadNurse()) {
+            List<NeedWard> needWards = needWardRepository.returnNeedWards();
+            List<Patient> patients = nurseService.getPatientsFromNeedWard(needWards);
             return ResponseEntity.ok(needWards);
-        }
-        else{
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
 
     @GetMapping("/get/all/nurse")
-    public ResponseEntity<List<Nurse>> getAllNurse(@RequestHeader("Authorization") String jwt){
-        String userName=JwtProvider.getUserNameFromJwtToken(jwt);
-        User user=userRepository.findByUserName(userName);
-        if(user.getRole().equals("admin")){
-            List<Nurse> nurses=nurseRepository.findAll();
+    public ResponseEntity<List<Nurse>> getAllNurse(@RequestHeader("Authorization") String jwt) {
+        String userName = JwtProvider.getUserNameFromJwtToken(jwt);
+        User user = userRepository.findByUserName(userName);
+        if (user.getRole().equals("admin")) {
+            List<Nurse> nurses = nurseRepository.findAll();
             return ResponseEntity.ok().body(nurses);
-        }
-        else{
+        } else {
             return ResponseEntity.badRequest().body(null);
         }
     }
+
     @GetMapping("get/all/available/wards")
-    ResponseEntity<List<Ward>> getAllAvailableWards(@RequestHeader("Authorization") String jwt){
-        String role=JwtProvider.getRoleFromJwtToken(jwt);
-        String userName=JwtProvider.getUserNameFromJwtToken(jwt);
-        User user=userRepository.findByUserName(userName);
-        Nurse nurse=nurseRepository.findByUser(user);
-        if(role.equals("nurse") && nurse.isHeadNurse()){
-            List<Ward> wards=wardRepository.findAvailableWard();
+    ResponseEntity<List<Ward>> getAllAvailableWards(@RequestHeader("Authorization") String jwt) {
+        String role = JwtProvider.getRoleFromJwtToken(jwt);
+        String userName = JwtProvider.getUserNameFromJwtToken(jwt);
+        User user = userRepository.findByUserName(userName);
+        Nurse nurse = nurseRepository.findByUser(user);
+        if (role.equals("nurse") && nurse.isHeadNurse()) {
+            List<Ward> wards = wardRepository.findAvailableWard();
             return ResponseEntity.ok().body(wards);
-        }
-        else{
+        } else {
             return ResponseEntity.badRequest().body(null);
         }
 
     }
 
     @GetMapping("/assign/ward/{wardId}/{needWardId}")
-    ResponseEntity<Ward> assignWard(@RequestHeader("Authorization") String jwt, @PathVariable Long wardId, @PathVariable Long needWardId) {
+    ResponseEntity<Ward> assignWard(@RequestHeader("Authorization") String jwt, @PathVariable Long wardId,
+            @PathVariable Long needWardId) {
         String role = JwtProvider.getRoleFromJwtToken(jwt);
         String userName = JwtProvider.getUserNameFromJwtToken(jwt);
         User user = userRepository.findByUserName(userName);
@@ -124,53 +125,56 @@ public class NurseController {
     }
 
     @PutMapping("/update/assigned/ward/patient/details/{wardId}")
-    ResponseEntity<String>updateAssignedWardPatientDetails(@RequestHeader("Authorization") String jwt, @PathVariable Long wardId, @RequestBody WardPatientDetails wardPatientDetails){
+    ResponseEntity<String> updateAssignedWardPatientDetails(@RequestHeader("Authorization") String jwt,
+            @PathVariable Long wardId, @RequestBody WardPatientDetails wardPatientDetails) {
         String role = JwtProvider.getRoleFromJwtToken(jwt);
-        if(role.equals("nurse")){
-            Optional<Patient> patientO = patientRepository.findById(wardRepository.findById(wardId).get().getPatient().getId());
-            if(patientO.isPresent()){
+        if (role.equals("nurse")) {
+            Optional<Patient> patientO = patientRepository
+                    .findById(wardRepository.findById(wardId).get().getPatient().getId());
+            if (patientO.isPresent()) {
                 Patient patient = patientO.get();
                 patient.setTemperature(wardPatientDetails.getTemperature());
                 patient.setBloodPressure(wardPatientDetails.getBloodPressure());
                 patient.setWeight(wardPatientDetails.getWeight());
                 patientRepository.save(patient);
                 return ResponseEntity.ok("Updated");
-            }
-            else{
+            } else {
                 return ResponseEntity.ok("Failed");
             }
-        }
-        else{
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access Denied");
         }
     }
 
     @GetMapping("/is/head/nurse/{nurseId}")
-    ResponseEntity<String>isNurseIsAHeadNurse(@RequestHeader("Authorization")String jwt,@PathVariable Long nurseId){
+    ResponseEntity<Map<String, String>> isNurseIsAHeadNurse(@RequestHeader("Authorization") String jwt,
+            @PathVariable Long nurseId) {
+        Map<String, String> response = new HashMap<>();
         try {
             String role = JwtProvider.getRoleFromJwtToken(jwt);
             if (role.equals("nurse")) {
                 Optional<Nurse> isNurse = nurseRepository.findById(nurseId);
-                if(isNurse.isPresent()){
+                if (isNurse.isPresent()) {
                     Nurse nurse = isNurse.get();
                     boolean isHeadNurse = nurse.isHeadNurse();
-                    if(isHeadNurse){
-                        return ResponseEntity.ok("yes");
+                    if (isHeadNurse) {
+                        response.put("message", "yes");
+                        return ResponseEntity.ok(response);
+                    } else {
+                        response.put("message", "no");
+                        return ResponseEntity.ok(response);
                     }
-                    else{
-                        return ResponseEntity.ok("no");
-                    }
+                } else {
+                    response.put("message", "yes");
+                    return ResponseEntity.ok(response);
                 }
-                else{
-                    return ResponseEntity.ok("No nurse present with the given Id");
-                }
+            } else {
+                response.put("message", "Access Denied");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
             }
-            else{
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access Denied");
-            }
-        }
-        catch(Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: "+e.getMessage());
+        } catch (Exception e) {
+            response.put("message", "Error: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 
